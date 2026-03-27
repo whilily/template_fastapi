7ea4914f34843ac004652b9c39d66f257f42317c
whilily <glassponzu@gmail.com>
2026-03-27 22:26:01 +0900

    update css.

--- templates/script.js
+++ templates/script.js
@@ -2,4 +2,4 @@ function fbtn_click(){
   document.getElmentById("lis").appendChild(document.createTextNode("Oh! What a wonderful world!"))
 }
 
-//document.getElementById("fbtn").addEventListener("click", fbtn_click)
+document.getElementById("fbtn").addEventListener("click", fbtn_click)
