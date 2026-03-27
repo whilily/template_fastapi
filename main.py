c2d4d595bb30deea8bff3283791617838fcc368a
whilily <glassponzu@gmail.com>
2026-03-27 20:45:18 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from typing import Optional
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import datetime
 
 app = FastAPI()
 templates = Jinja2Templates(directory="./")
 
-
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html",{"request":request})
+    return templates.TemplateResponse("index.html",{"request":request, "launch_time":datetime.datetime.now()})
