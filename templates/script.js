bc73d8547f25e116e3f77bbeea4d2690e1227b57
whilily <glassponzu@gmail.com>
2026-03-27 21:56:22 +0900

    Update script.js

--- templates/script.js
+++ templates/script.js
@@ -1,4 +1,4 @@
 function fbtn_click():
   document.getElementById("lis").appendChild(document.createTextNode("Oh! What a wonderful world!"))
 
-document.getElementById("fbtn").addEventListener("click", fbtn_click)
+//document.getElementById("fbtn").addEventListener("click", fbtn_click)
