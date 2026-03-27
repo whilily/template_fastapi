e5dfbf70193f44a32ea87cdb08a539b43db18cc5
whilily <glassponzu@gmail.com>
2026-03-27 22:07:22 +0900

    fixed errors.

--- templates/script.js
+++ templates/script.js
@@ -1,4 +1,5 @@
-function fbtn_click():
-  document.getElementById("lis").appendChild(document.createTextNode("Oh! What a wonderful world!"))
+function fbtn_click(){
+  document.getElmentById("lis").appendChild(document.createTextNode("Oh! What a wonderful world!"))
+}
 
 //document.getElementById("fbtn").addEventListener("click", fbtn_click)
