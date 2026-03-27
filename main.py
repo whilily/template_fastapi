37ebd5741e0d29a0b536da6363e47f632f6e404c
whilily <glassponzu@gmail.com>
2026-03-27 21:05:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from fastapi.templating import Jinja2Templates
 import datetime
 
 app = FastAPI()
-templates = Jinja2Templates(directory="./")
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
 async def root(request: Request):
